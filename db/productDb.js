6d4965ad54428258dbef3dcb73df451405165232
gurungbsal18 <gurungbsal18@gmail.com>
2025-04-02 19:04:07 +0545

    js file created

---
+++ db/productDb.js
@@ -0,0 +1,14 @@
+const productDb = [
+  {
+    id: 1,
+    title: "Atom Mechanical Keyboard",
+    price: "2500",
+  },
+  {
+    id: 2,
+    title: "MSI Monitor 24",
+    price: "24500",
+  },
+];
+
+const testTitle = document.getElementById("products");
